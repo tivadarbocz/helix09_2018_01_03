bb29ab813a94f69e418552de38621ed0e8959a2c
bocz.tivadar@lanoga.hu <bocz.tivadar@lanoga.hu>
2018-01-03 17:53:00 +0100

    1-2

--- src/hu/tb/FileViewer.java
+++ src/hu/tb/FileViewer.java
@@ -1,27 +1,63 @@
 package hu.tb;
 
-import java.io.File;
-
+import java.io.*;
 
 public class FileViewer {
 	private File folder;
 	private File[] listOfFiles;
-
-	public FileViewer() {
-
-	}
+	private File lastPrintedFile;
 
 	public FileViewer(File folder) {
 		this.folder = folder;
+		listOfFiles = folder.listFiles();
 	}
 
-	//TODO file-ok listázása név + méret kb-ban
+	// file-ok listázása név + méret kb-ban
 	//TODO rendezés méret szerint, mely paraméterezhető, hogy csökkenő vagy növekvő
 	public void listFilesInDir(){
-		listOfFiles = folder.listFiles();
 		for(File file : listOfFiles){
-			System.out.println("name: " + " size: " + " kb");
+			System.out.println("name: " + file.getName() + " size: " + file.length() / 1024.0 +  " kb");
 		}
+	}
+
+	public void printNextFileContent(){
+
+	}
+
+	public void printPreviousFileContent(){
+
+	}
+
+	/**
+	 * Kiírja az első *.txt tartalmát console-ra
+	 */
+	public void printFirstTextFile(){
+		lastPrintedFile = searchFirstTextFile();
+		printFileContent(lastPrintedFile);
+
+	}
+
+	public void printFileContent(File file){
+		try(BufferedReader br = new BufferedReader(new FileReader(file))){
+			String line;
+			while((line = br.readLine()) != null){
+				System.out.println(line);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private File searchFirstTextFile(){
+		for(File file : listOfFiles){
+			if(file.getName().endsWith("txt")){
+				return file;
+			}
+		}
+
+		return null;
 	}
 
 
--- src/hu/tb/Menu.java
+++ src/hu/tb/Menu.java
@@ -1,29 +1,74 @@
 package hu.tb;
 
-import java.io.File;
+import java.util.Scanner;
+import java.io.*;
 
 //TODO refactor to singleton
 public class Menu {
 
+	private FileViewer fileViewer;
+
 	public Menu() {
+		fileViewer = new FileViewer(new File("C:\\Users\\Admin\\Desktop\\resources"));
 	}
 
 	//TODO Console-os menü megvalósítása
-	public void run(){
-		FileViewer fileViewer = new FileViewer(new File("C:\\Users\\Admin\\Desktop\\resources"));
-		fileViewer.listFilesInDir();
+	public void run() {
+
+		int flag = -1;
+
+		while (flag != 0) {
+			printMenu();
+			System.out.println("Menü pont kiválasztása: ");
+			Scanner scanner = new Scanner(System.in);
+			flag = scanner.nextInt();
+
+			switch (flag) {
+			case 1:
+				fileViewer.listFilesInDir();
+				break;
+			case 2:
+				fileViewer.printFirstTextFile();
+				break;
+			case 3:
+				//TODO
+				break;
+			case 4:
+				//TODO
+				break;
+			case 5:
+				//ODO
+				break;
+			case 6:
+				//TODO
+				break;
+			case 7:
+				//TODO
+				break;
+			case 8:
+				//TODO
+				break;
+			}
+			System.out.println("\n");
+
+		}
 	}
 
-	// TODO választható menüpontok
-	// TODO - 1. *.txt tartalmának a megnyitása(console-ra írása)
+	//  választható menüpontok
+	//  - 1. *.txt tartalmának a megnyitása(console-ra írása)
 	// TODO - Következő *.txt tartalmának a megnyitása(console-ra írása)
 	// TODO - Előző *.txt tartalmának a megnyitása(console-ra írása)
 	// TODO - Név alapján *.txt tartalmának a megnyitása(console-ra írása)
-	// TODO - Kilépés
 	// TODO - file létrehozás név + tartalom
 	// TODO - file tartalom módosítás név alapján
 	// TODO - file törlése
 	// TODO - legnagyobb méretű file megkeresése, tartalmának elfelezése és 2 külön file-ba írása
+	// TODO - Kilépés
 
-
+	private void printMenu() {
+		System.out.println("**********Menu***********");
+		System.out.println("**File-ok listázása(1)***");
+		System.out.println("** 1 txt file kiírása(2)*");
+		System.out.println("*******Kilépés (0)******");
+	}
 }
